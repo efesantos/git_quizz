ffa8fc2b64c00299a68a2c12ea768e88b1587e69
Elmer <efe_santos@hotmail.com>
2016-04-22 14:21:40 +0000

    Create table cats

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitQuizz::Application.config.secret_key_base = '5f60024a38ae5242ece9c802f6c1ebbaa7da5b97b2be745776fc635f00b6c72bf269ad2f347ca14a92887b113b2edfd94d19ff0b92fbecb771b2a92b81421044'
